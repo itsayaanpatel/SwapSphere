2acef3b73c6db20e262633a7efdf58a4be85007a
Ayaan Patel <ayaan2006patel@gmail.com>
2025-04-15 18:48:00 -0400

    Mock Data SQL files

---
+++ database-files/Items.sql
@@ -0,0 +1,102 @@
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (1, 24, 'Potting Soil', 'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.
+
+In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.
+
+Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.', 'Home Goods', 125355, 'Traded', '3/13/2025');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (2, 14, 'Organic Granola', 'In congue. Etiam justo. Etiam pretium iaculis justo.
+
+In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.
+
+Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', 'Sports', 825140, 'Available', '5/25/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (3, 10, 'Salt and Pepper Grinder Set', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', 'Sports', 916714, 'Traded', '4/20/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (4, 30, 'Camping Chair with Cooler', 'Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.', 'Home Goods', 704742, 'Traded', '9/21/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (5, 18, 'Foam Roller for Muscle Recovery', 'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.
+
+Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.
+
+Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.', 'Collectibles', 674205, 'Available', '1/13/2025');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (6, 24, 'Marinara Sauce', 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
+
+Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.
+
+Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.', 'Books', 112791, 'Pending', '12/22/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (7, 4, 'Set of Silicone Cooking Utensils', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.
+
+Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.', 'Electronics', 738547, 'Available', '12/27/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (8, 8, 'Smart Doorbell', 'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.', 'Books', 904943, 'Traded', '11/18/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (9, 28, 'Dark Chocolate Tart', 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.
+
+Phasellus in felis. Donec semper sapien a libero. Nam dui.
+
+Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.', 'Books', 269614, 'Pending', '11/9/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (10, 10, 'Cordless Electric Screwdriver', 'In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.
+
+Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.
+
+Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.', 'Collectibles', 978287, 'Available', '1/11/2025');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (11, 17, 'Bluetooth Tracker', 'Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.
+
+Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.', 'Books', 161297, 'Traded', '8/5/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (12, 5, 'Air Purifier', 'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.
+
+Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.
+
+Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', 'Collectibles', 363929, 'Pending', '3/4/2025');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (13, 2, 'Cream Cheese', 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', 'Equipment', 841954, 'Pending', '5/25/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (14, 3, 'Creamy Garlic Mashed Potatoes', 'Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.
+
+In congue. Etiam justo. Etiam pretium iaculis justo.
+
+In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.', 'Gaming', 399976, 'Pending', '10/25/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (15, 8, 'Ground Cinnamon', 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+
+Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.
+
+Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.', 'Sneakers', 427261, 'Pending', '2/15/2025');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (16, 29, 'Rechargeable Electric Toothbrush', 'Sed ante. Vivamus tortor. Duis mattis egestas metus.
+
+Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.
+
+Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.', 'Clothing', 558913, 'Pending', '7/15/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (17, 5, 'Kale Caesar Salad Kit', 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.', 'Home Goods', 593033, 'Pending', '6/11/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (18, 17, 'Chicken Broth', 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 'Clothing', 666458, 'Traded', '9/20/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (19, 14, 'Stainless Steel BBQ Grill Set', 'Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.
+
+In congue. Etiam justo. Etiam pretium iaculis justo.
+
+In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.', 'Clothing', 550701, 'Available', '11/22/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (20, 23, 'Tea Set with Infuser', 'In congue. Etiam justo. Etiam pretium iaculis justo.
+
+In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.
+
+Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', 'Books', 484744, 'Traded', '10/28/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (21, 18, 'Peach & Mango Salsa', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.
+
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', 'Gaming', 250979, 'Traded', '11/27/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (22, 30, 'Overnight Hiking Backpack', 'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.', 'Gaming', 736628, 'Pending', '11/12/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (23, 15, 'LED Strip Light Kit', 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.
+
+Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.', 'Books', 286221, 'Pending', '2/20/2025');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (24, 10, 'Baby Monitor', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
+
+Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
+
+In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.', 'Gaming', 261725, 'Traded', '11/22/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (25, 20, 'Berries Medley', 'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.
+
+Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.
+
+Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.', 'Equipment', 747156, 'Pending', '11/28/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (26, 25, 'Chunky Knit Sweater', 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', 'Equipment', 454049, 'Traded', '10/20/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (27, 9, 'Saffron Rice Mix', 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.
+
+Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.
+
+Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', 'Gaming', 510716, 'Pending', '5/15/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (28, 18, 'Handmade Leather Journal', 'Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.
+
+Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.', 'Equipment', 863623, 'Pending', '5/13/2024');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (29, 26, 'Baking Soda', 'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.', 'Clothing', 480126, 'Available', '3/14/2025');
+insert into Items (item_id, user_id, title, description, category, estimated_value, status, created_at) values (30, 24, 'Craft Beer Mustard', 'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.', 'Electronics', 763650, 'Available', '5/9/2024');
