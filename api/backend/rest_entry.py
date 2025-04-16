56aef81b0fde858adcd27e9522e0d17050cc022f
SulemanSheikh1 <sheikh.su@northeastern.edu>
2025-04-16 15:16:36 -0400

    MORE BLUEPRINTS

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -26,7 +26,7 @@ from api.backend.items.item_routes         import items
 from backend.trades.trade_routes           import trades
 
 # admin (users, fraud, logs, analytics, ML)
-from backend.admin.admin_routes      import admin_bp
+from backend.admin.admin_routes            import admin_bp
 from backend.analytics.analytics_routes    import analytics
 
 
@@ -71,6 +71,8 @@ def create_app():
     app.register.blueprint(items,       url_prefix='/i')
     app.register_blueprint(seller,      url_prefix='/s')
     app.register_blueprint(trades,      url_prefix='/t')
+    app.register_blueprint(analytics,   url_prefix='/a')
+    app.register_blueprint(admin_bp,    url_prefix='/ad')
 
     # Don't forget to return the app object
     return app
