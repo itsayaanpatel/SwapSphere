ab6f0538a3db447c84ccb0eb798621a2289271f8
Ayaan Patel <ayaan2006patel@gmail.com>
2025-04-16 21:42:43 -0400

    fixed abiguous routes

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -79,7 +79,7 @@ def create_app():
     app.register_blueprint(market_valuations, url_prefix='/')
     app.register_blueprint(seller,      url_prefix='/seller')
     app.register_blueprint(items,       url_prefix='/')
-    app.register_blueprint(trades,      url_prefix='/')
+    app.register_blueprint(trades,      url_prefix='/t')
     app.register_blueprint(cash_deals,      url_prefix='/')
     app.register_blueprint(analytics,   url_prefix='/')
     app.register_blueprint(admin_bp,    url_prefix='/')
